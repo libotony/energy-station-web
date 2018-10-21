f4a754fe281d24cadcf9a2eff9d996919ae15c7f
tony <tony.li@vechain.com>
2018-10-21 15:23:22 +0800

    remove vet token address(load from station contract)

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -6,8 +6,6 @@ import EnergyABI from './energy.json'
 const EnergyStationAddress = '0x7C2A6c88dEf4dcaD70716e0401423e73c2cF6b4d'
 const EnergyAddress = '0x0000000000000000000000000000456E65726779'
 
-const VETTokenAddress = '0xE4BE21E6a9B8f5cAdB1321f8Aa3CFf959EcF6F70'
-
 const findInABI = function (name: string, ABIDef: {[index: string]: any}[]) {
     const ABI = ABIDef.find((abi) => {
         if (abi.name === name) {
@@ -26,7 +24,6 @@ export {
     EnergyStationABI,
     EnergyABI,
     EnergyAddress,
-    VETTokenAddress,
     EnergyStationAddress,
     findInABI
 }
