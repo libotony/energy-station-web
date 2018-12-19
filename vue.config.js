04db68d0b461e23e114c8492e974978b74800e2e
tony <tony.li@vechain.com>
2018-12-19 14:09:39 +0800

    pwa: update settings

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,10 @@
 module.exports = {
     lintOnSave: false,
-
     configureWebpack: {
         node: {
             Buffer: true
         }
     },
-
     baseUrl: process.env.NODE_ENV === 'production' ? '/energy-station/': '/',
-    outputDir: undefined,
-    assetsDir: undefined,
-    runtimeCompiler: undefined,
-    productionSourceMap: undefined,
-    parallel: undefined,
-    css: undefined
 }
 
