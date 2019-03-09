56e4c62932dfc712d272e6dd75242e0db2221c23
tony <tony.li@vechain.com>
2019-03-09 21:21:29 +0800

    remove unused options for pwa

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,6 @@ module.exports = {
     baseUrl: '/',
     pwa: {
         workboxOptions: {
-            navigateFallback: '/404.html',
             skipWaiting: true,
             clientsClaim: true
         }
