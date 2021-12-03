274ed48f8137284f998b5468127fe33c74ab6014
AsbertMa <stayinalive@msn.cn>
2021-12-03 11:29:54 +0800

    connex replace by _connex

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faAngleDoubleUp, faAngleDoubleDown, faCircleNotch, faCheckCircle, faInfoCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { Connex } from '@vechain/connex'
 import './registerServiceWorker'
 
 library.add(faAngleDoubleUp, faAngleDoubleDown, faCircleNotch, faCheckCircle, faInfoCircle, faTimesCircle)
@@ -13,9 +14,22 @@ Vue.component('fa-i', FontAwesomeIcon)
 import App from './App.vue'
 import router from './router'
 
+declare global {
+    interface Window {
+        _connex: Connex
+    }
+}
+
+Object.defineProperty(window, '_connex', {
+    value: new Connex({
+        node: 'https://sync-testnet.veblocks.net',
+        network: 'test'
+    }),
+    writable: false
+})
+
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-
 new Vue({
     router,
     render: (h) => h(App)
